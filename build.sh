d6d7fc4352e77f98251f02b559546ac391059da0
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-12-19 09:15:57 -0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl  https://static.rust-lang.org/dist/rustc-1.29.0-src.tar.gz > rustc-1.29.0-src.tar.gz
-gzip -dc rustc-1.29.0-src.tar.gz
+gzip -dc rustc-1.29.0-src.tar.gz | tar xvf -
 make bin/mrustc
 make -f minicargo.mk bin/minicargo
 make -f minicargo.mk output/libcore.rlib
