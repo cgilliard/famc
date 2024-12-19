0229b6a4083d75ed01988fdfee5ab515142f3b31
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-12-19 09:19:38 -0800

    improve curl download

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-curl  https://static.rust-lang.org/dist/rustc-1.29.0-src.tar.gz > rustc-1.29.0-src.tar.gz
-gzip -dc rustc-1.29.0-src.tar.gz | tar xvf -
+
+if [ ! -d rustc-1.29.0-src ]; then
+  curl -s https://static.rust-lang.org/dist/rustc-1.29.0-src.tar.gz | tar -xzf -
+fi
+
 make bin/mrustc
 make -f minicargo.mk bin/minicargo
 make -f minicargo.mk output/libcore.rlib
