11b561320d82fd1c9ebf5aa3c4fe3becd3cf4695
Christopher Gilliard <christopher.gilliard@gmail.com>
2024-12-19 22:45:23 -0800

    download rustc src

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+RUSTC_SRC="rustc-`cat rust-version`-src";
+if [ ! -d ${RUSTC_SRC} ]; then
+  curl -s https://static.rust-lang.org/dist/${RUSTC_SRC}.tar.gz | tar -xzf -
+fi
+
 make bin/mrustc
 make -f minicargo.mk bin/minicargo
 make -f minicargo.mk output/libcore.rlib
