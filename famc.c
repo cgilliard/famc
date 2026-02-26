7054e10ea79f57a4e86a1fd69f6b547d01990078
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-02-25 22:28:56 -0800

    lexer with some conversion to conditional expressions

--- famc.c
+++ famc.c
@@ -102,6 +102,7 @@ enum node_kind {
 	nk_comma,
 	nk_colon,
 	nk_asterisk,
+	nk_questionmark,
 	nk_ampersand,
 	nk_pipe,
 	nk_dot,
@@ -197,7 +198,7 @@ void cstrlen(long *ret, char *x) {
 	char *y;
 	y = x;
 loop:
-	*x ? 0 : ({ goto end; });
+	*x ? ({}) : ({ goto end; });
 	x++;
 	goto loop;
 end:
@@ -207,7 +208,7 @@ end:
 
 void cstrcmp(long *ret, char *x, char *y) {
 loop:
-	*x == *y && *x ? 0 : ({ goto end; });
+	*x == *y && *x ? ({}) : ({ goto end; });
 	x++;
 	y++;
 	goto loop;
@@ -221,7 +222,7 @@ void cmemset(void *dest, char c, long n) {
 	char *tmp;
 	tmp = dest;
 loop:
-	n-- ? 0 : ({ goto end; });
+	n-- ? ({}) : ({ goto end; });
 	*tmp++ = (char)c;
 	goto loop;
 end:;
@@ -287,10 +288,11 @@ void write_num(long fd, long num) {
 	long len;
 	long written;
 	long neg;
+	long tmp;
 
 	neg = 0;
 	map((void *)&buf, 21);
-	buf ? 0 : panic("Could not allocate memory!");
+	buf ? ({}) : panic("Could not allocate memory!");
 
 	p = buf + 20;
 	*p = *"\0";
@@ -305,14 +307,13 @@ void write_num(long fd, long num) {
 		   : 0;
 
 loop:
-	num > 0 ? 0 : ({ goto end; });
-	long tmp;
+	num > 0 ? ({}) : ({ goto end; });
 	tmp = num % 10;
 	*--p = *"0" + tmp;
 	num = num / 10;
 	goto loop;
 end:
-	neg ? ({ *--p = *"-"; }) : 0;
+	neg ? ({ *--p = *"-"; }) : ({ 0; });
 	len = (buf + 20) - p;
 	write(&written, fd, p, len);
 	munmap(buf, 21);
@@ -321,7 +322,7 @@ end:
 void arena_init(struct arena **a, long size, long align) {
 	struct arena *ret;
 	map((void *)&ret, size + sizeof(struct arena));
-	ret ? 0 : panic("Could not mmap sufficient memory! Halting.\n");
+	ret ? ({}) : panic("Could not mmap sufficient memory! Halting.\n");
 	ret->start = ((char *)(ret)) + sizeof(struct arena);
 	ret->end = ret->start + size;
 	(long)ret->start % align ? ({
@@ -339,23 +340,677 @@ void arena_alloc(void **result, struct arena *a, long size) {
 	to_alloc = (size + (a->align - 1)) & ~(a->align - 1);
 	a->current > a->end - to_alloc || to_alloc > (long)a->end
 	    ? panic("Could not allocate memory! Halting.\n")
-	    : 0;
+	    : ({});
 
 	ret = a->current;
 	a->current += to_alloc;
 	*result = ret;
 }
 
-void cmain(long argc, char **argv) {
-	char arr[20];
-	long i;
-	i = 4;
+void lexer_next_token(struct node *next, struct lexer *l) {
+	char *in;
+	in = l->in + l->off;
 
-	write_num(2, i);
+begin1:
+	in == l->end ? ({ goto end1; }) : ({});
+	(*in - 9) >= 5 && *in != *" " ? ({ goto end1; }) : ({});
+	(*in == *"\n") ? ({
+		l->col_start = (in - l->in) + 1;
+		l->line++;
+	})
+		       : 0;
+	in++;
+	goto begin1;
+end1:
+	l->off = in - l->in;
+	next->loc.off = l->off;
+	next->loc.line = l->line;
+	next->loc.col = l->off - l->col_start;
+	if (in == l->end) {
+		next->loc.len = 0;
+		next->kind = nk_term;
+	} else if (*in == *";") {
+		next->loc.len = 1;
+		next->kind = nk_semi;
+		l->off++;
+	} else if (*in == *",") {
+		next->loc.len = 1;
+		next->kind = nk_comma;
+		l->off++;
+	} else if (*in == *":") {
+		next->loc.len = 1;
+		next->kind = nk_colon;
+		l->off++;
+	} else if (*in == *"*") {
+		next->loc.len = 1;
+		next->kind = nk_asterisk;
+		l->off++;
+	} else if (*in == *"?") {
+		next->loc.len = 1;
+		next->kind = nk_questionmark;
+		l->off++;
+	} else if (*in == *"%") {
+		next->loc.len = 1;
+		next->kind = nk_mod;
+		l->off++;
+	} else if (*in == *"~") {
+		next->loc.len = 1;
+		next->kind = nk_tilde;
+		l->off++;
+	} else if (*in == *"|") {
+		if (++in != l->end && *in == *"|") {
+			next->loc.len = 2;
+			next->kind = nk_double_pipe;
+			l->off += 2;
+		} else {
+			next->loc.len = 1;
+			next->kind = nk_pipe;
+			l->off++;
+		}
+	} else if (*in == *".") {
+		next->loc.len = 1;
+		next->kind = nk_dot;
+		l->off++;
+	} else if (*in == *"&") {
+		if (++in != l->end && *in == *"&") {
+			next->loc.len = 2;
+			next->kind = nk_double_ampersand;
+			l->off += 2;
+		} else {
+			next->loc.len = 1;
+			next->kind = nk_ampersand;
+			l->off++;
+		}
+	} else if (*in == *"=") {
+		if (++in != l->end && *in == *"=") {
+			next->loc.len = 2;
+			next->kind = nk_double_equal;
+			l->off += 2;
+		} else {
+			next->loc.len = 1;
+			next->kind = nk_equal;
+			l->off++;
+		}
+	} else if (*in == *"(") {
+		next->loc.len = 1;
+		next->kind = nk_left_paren;
+		l->off++;
+	} else if (*in == *")") {
+		next->loc.len = 1;
+		next->kind = nk_right_paren;
+		l->off++;
+	} else if (*in == *"{") {
+		next->loc.len = 1;
+		next->kind = nk_left_brace;
+		l->off++;
+	} else if (*in == *"}") {
+		next->loc.len = 1;
+		next->kind = nk_right_brace;
+		l->off++;
+	} else if (*in == *"[") {
+		next->loc.len = 1;
+		next->kind = nk_left_bracket;
+		l->off++;
+	} else if (*in == *"]") {
+		next->loc.len = 1;
+		next->kind = nk_right_bracket;
+		l->off++;
+	} else if (*in == *"-") {
+		if (++in != l->end) {
+			if (*in == *"=") {
+				next->loc.len = 2;
+				next->kind = nk_hyphen_equal;
+				l->off += 2;
+				goto end;
+			} else if (*in == *"-") {
+				next->loc.len = 2;
+				next->kind = nk_double_hyphen;
+				l->off += 2;
+				goto end;
+			} else if (*in == *">") {
+				next->loc.len = 2;
+				next->kind = nk_arrow;
+				l->off += 2;
+				goto end;
+			}
+		}
+		next->loc.len = 1;
+		next->kind = nk_hyphen;
+		l->off++;
+	} else if (*in == *"+") {
+		if (++in != l->end) {
+			if (*in == *"=") {
+				next->loc.len = 2;
+				next->kind = nk_plus_equal;
+				l->off += 2;
+				goto end;
+			} else if (*in == *"+") {
+				next->loc.len = 2;
+				next->kind = nk_double_plus;
+				l->off += 2;
+				goto end;
+			}
+		}
+		next->loc.len = 1;
+		next->kind = nk_plus;
+		l->off++;
+	} else if (*in == *"<") {
+		if (++in != l->end && *in == *"=") {
+			next->loc.len = 2;
+			next->kind = nk_lte;
+			l->off += 2;
+		} else {
+			next->loc.len = 1;
+			next->kind = nk_lt;
+			l->off++;
+		}
+	} else if (*in == *">") {
+		if (++in != l->end && *in == *"=") {
+			next->loc.len = 2;
+			next->kind = nk_gte;
+			l->off += 2;
+		} else {
+			next->loc.len = 1;
+			next->kind = nk_gt;
+			l->off++;
+		}
+	} else if (*in == *"!") {
+		if (++in != l->end && *in == *"=") {
+			next->loc.len = 2;
+			next->kind = nk_ne;
+			l->off += 2;
+		} else {
+			next->loc.len = 1;
+			next->kind = nk_bang;
+			l->off++;
+		}
+	} else if (*in == *"\"") {
+		while (++in != l->end) {
+			if (*in == *"\"") {
+				if (*(in - 1) != *"\\") break;
+				if (*(in - 2) == *"\\") break;
+			} else if (*in == *"\n") {
+				l->col_start = (in - l->in) + 1;
+				l->line++;
+			}
+		}
+		if (in != l->end) {
+			in++;
+			next->loc.len = in - (l->in + l->off);
+			next->kind = nk_str_lit;
+			l->off += next->loc.len;
+		} else {
+			next->loc.len = in - (l->in + l->off);
+			next->kind = nk_error;
+			l->off += next->loc.len;
+		}
+	} else if (*in == *"/") {
+		if (++in == l->end || *in != *"*") {
+			next->loc.len = 1;
+			next->kind = nk_div;
+			l->off++;
+		} else {
+			while (++in != l->end) {
+				if (*in == *"/" && *(in - 1) == *"*") break;
+				if (*in == *"\n") {
+					l->col_start = (in - l->in) + 1;
+					l->line++;
+				}
+			}
+			if (in == l->end) {
+				next->loc.len = in - (l->in + l->off);
+				next->kind = nk_error;
+				l->off += next->loc.len;
+			} else {
+				next->loc.len = in - (l->in + l->off) + 1;
+				next->kind = nk_comment;
+				l->off += next->loc.len;
+			}
+		}
+	} else if (*in == *"_") {
+		long ch1;
+		long ch2;
+		long ch3;
+		if (++in == l->end || *in != *"_") goto ident;
+		if (++in == l->end || *in != *"a") goto ident;
+		if (++in == l->end || *in != *"s") goto ident;
+		if (++in == l->end || *in != *"m") goto ident;
+		if (++in == l->end || *in != *"_") goto ident;
+		if (++in == l->end || *in != *"_") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_asm;
+		l->off += next->loc.len;
+	} else if (*in == *"b") {
+		long ch1;
+		long ch2;
+		long ch3;
+		if (++in == l->end || *in != *"r") goto ident;
+		if (++in == l->end || *in != *"e") goto ident;
+		if (++in == l->end || *in != *"a") goto ident;
+		if (++in == l->end || *in != *"k") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_break;
+		l->off += next->loc.len;
+	} else if (*in == *"c") {
+		long ch1;
+		long ch2;
+		long ch3;
+		if (++in == l->end || *in != *"h") goto ident;
+		if (++in == l->end || *in != *"a") goto ident;
+		if (++in == l->end || *in != *"r") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_char;
+		l->off += next->loc.len;
+	} else if (*in == *"e") {
+		long ch1;
+		long ch2;
+		long ch3;
+		if (++in == l->end || *in != *"l") {
+			if (in != l->end && *in == *"n") {
+				if (++in == l->end || *in != *"u") goto ident;
+				if (++in == l->end || *in != *"m") goto ident;
+
+				if (++in != l->end) {
+					ch1 = (*in - *"a") & 0xFF;
+					ch2 = (*in - *"A") & 0xFF;
+					ch3 = (*in - *"0") & 0xFF;
+					if (ch1 < 26 || ch2 < 26 || ch3 < 10 ||
+					    *in == *"_")
+						goto ident;
+				}
+
+				next->loc.len = (long)(in - (l->in + l->off));
+				next->kind = nk_enum;
+				l->off += next->loc.len;
+				goto end;
+			}
+
+			goto ident;
+		}
+		if (++in == l->end || *in != *"s") goto ident;
+		if (++in == l->end || *in != *"e") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_else;
+		l->off += next->loc.len;
+	} else if (*in == *"g") {
+		long ch1;
+		long ch2;
+		long ch3;
+
+		if (++in == l->end || *in != *"o") goto ident;
+		if (++in == l->end || *in != *"t") goto ident;
+		if (++in == l->end || *in != *"o") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_goto;
+		l->off += next->loc.len;
+	} else if (*in == *"i") {
+		long ch1;
+		long ch2;
+		long ch3;
+
+		if (++in == l->end || *in != *"f") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_if;
+		l->off += next->loc.len;
+	} else if (*in == *"l") {
+		long ch1;
+		long ch2;
+		long ch3;
+
+		if (++in == l->end || *in != *"o") goto ident;
+		if (++in == l->end || *in != *"n") goto ident;
+		if (++in == l->end || *in != *"g") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_long;
+		l->off += next->loc.len;
+	} else if (*in == *"s") {
+		long ch1;
+		long ch2;
+		long ch3;
+
+		if (++in == l->end || *in != *"i") {
+			if (in != l->end && *in == *"t") {
+				if (++in == l->end || *in != *"r") goto ident;
+				if (++in == l->end || *in != *"u") goto ident;
+				if (++in == l->end || *in != *"c") goto ident;
+				if (++in == l->end || *in != *"t") goto ident;
+
+				if (++in != l->end) {
+					ch1 = (*in - *"a") & 0xFF;
+					ch2 = (*in - *"A") & 0xFF;
+					ch3 = (*in - *"0") & 0xFF;
+					if (ch1 < 26 || ch2 < 26 || ch3 < 10 ||
+					    *in == *"_")
+						goto ident;
+				}
+
+				next->loc.len = (long)(in - (l->in + l->off));
+				next->kind = nk_struct;
+				l->off += next->loc.len;
+				goto end;
+			}
+			goto ident;
+		}
+		if (++in == l->end || *in != *"z") goto ident;
+		if (++in == l->end || *in != *"e") goto ident;
+		if (++in == l->end || *in != *"o") goto ident;
+		if (++in == l->end || *in != *"f") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_sizeof;
+		l->off += next->loc.len;
+
+	} else if (*in == *"v") {
+		long ch1;
+		long ch2;
+		long ch3;
+
+		if (++in == l->end || *in != *"o") goto ident;
+		if (++in == l->end || *in != *"i") goto ident;
+		if (++in == l->end || *in != *"d") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_void;
+		l->off += next->loc.len;
+	} else if (*in == *"w") {
+		long ch1;
+		long ch2;
+		long ch3;
+
+		if (++in == l->end || *in != *"h") goto ident;
+		if (++in == l->end || *in != *"i") goto ident;
+		if (++in == l->end || *in != *"l") goto ident;
+		if (++in == l->end || *in != *"e") goto ident;
+
+		if (++in != l->end) {
+			ch1 = (*in - *"a") & 0xFF;
+			ch2 = (*in - *"A") & 0xFF;
+			ch3 = (*in - *"0") & 0xFF;
+			if (ch1 < 26 || ch2 < 26 || ch3 < 10 || *in == *"_")
+				goto ident;
+		}
+
+		next->loc.len = (long)(in - (l->in + l->off));
+		next->kind = nk_while;
+		l->off += next->loc.len;
+	} else {
+		long ch1;
+		long ch2;
+		long ch3;
+
+		ch1 = (*in - *"a") & 0xFF;
+		ch2 = (*in - *"A") & 0xFF;
+
+		if (ch1 < 26 || ch2 < 26 || *in == *"_") {
+		ident:
+			in = l->in + l->off;
+			while (++in != l->end) {
+				ch1 = (*in - *"a") & 0xFF;
+				ch2 = (*in - *"A") & 0xFF;
+				ch3 = (*in - *"0") & 0xFF;
+				if (!(ch1 < 26 || ch2 < 26 || ch3 < 10 ||
+				      *in == *"_"))
+					break;
+			}
+			next->loc.len = (long)(in - (l->in + l->off));
+			next->kind = nk_ident;
+			l->off += next->loc.len;
+			goto end;
+		} else {
+			ch1 = (*in - *"0") & 0xFF;
+
+			if (ch1 < 10) {
+				long is_hex;
+				long is_bin;
+				is_hex = 0;
+				is_bin = 0;
+				if (in + 1 != l->end && *(in + 1) == *"x" &&
+				    *in == *"0") {
+					is_hex = 1;
+					in++;
+				} else if (in + 1 != l->end &&
+					   *(in + 1) == *"b" && *in == *"0") {
+					is_bin = 1;
+					in++;
+				}
+				while (++in != l->end) {
+					ch1 = (*in - *"0") & 0xFF;
+					if (is_bin && ch1 > 1)
+						break;
+					else if (ch1 >= 10) {
+						if (is_hex) {
+							ch1 =
+							    (*in - *"a") & 0xFF;
+							ch2 =
+							    (*in - *"A") & 0xFF;
+							if (ch1 > 5 && ch2 > 5)
+								break;
+						} else
+							break;
+					}
+				}
+				next->loc.len = (long)(in - (l->in + l->off));
+				next->kind = nk_num_lit;
+				l->off += next->loc.len;
+				goto end;
+			}
+		}
+		next->loc.len = 1;
+		next->kind = nk_error;
+		l->off++;
+	}
+end:;
+}
+
+void node_init(struct parser *p, struct node **n, enum node_kind kind) {
+	arena_alloc((void *)n, p->a, sizeof(struct node));
+	cmemset(*n, 0, sizeof(struct node));
+	(*n)->kind = kind;
+}
+
+void node_print(struct parser *p, struct node *n) {
+	(void)p;
+	(void)n;
+}
+
+void print_error(struct node *n, char *msg) {
+	write_str(2, "Error: ");
+	if (n) {
+		write_num(2, 1 + n->loc.line);
+		write_str(2, ":");
+		write_num(2, 1 + n->loc.col);
+		write_str(2, ": ");
+	}
+	write_str(2, msg);
 	write_str(2, "\n");
+	exit_group(-1);
+}
+
+void proc_left_paren(struct parser *p) { p->sp = 0; }
+void proc_right_paren(struct parser *p) { p->sp = 0; }
+void proc_left_brace(struct parser *p) { p->sp = 0; }
+void proc_right_brace(struct parser *p) { p->sp = 0; }
+void proc_semi(struct parser *p) { p->sp = 0; }
+
+void parse(struct parser *p, struct lexer *l, long debug) {
+	while (1) {
+		if (p->sp >= p->capacity) panic("Stack overflow!");
+		lexer_next_token(&p->stack[p->sp], l);
+		if (p->stack[p->sp].kind == nk_error)
+			print_error(&p->stack[p->sp], "unrecognized token");
+		if (debug && p->stack[p->sp].kind <= nk_term) {
+			long r;
+			write_str(1, "token=");
+			write_num(1, p->stack[p->sp].kind);
+			write_str(1, ",offset=");
+			write_num(1, p->stack[p->sp].loc.off);
+			write_str(1, ",value='");
+			write(&r, 1, l->in + p->stack[p->sp].loc.off,
+			      p->stack[p->sp].loc.len);
+			write_str(1, "',line=");
+			write_num(1, p->stack[p->sp].loc.line + 1);
+			write_str(1, ",col=");
+			write_num(1, p->stack[p->sp].loc.col + 1);
+			write_str(1, "\n");
+		}
+
+		if (p->stack[p->sp].kind == nk_comment) continue;
+		if (p->stack[p->sp].kind == nk_term) {
+			break;
+		} else {
+			enum node_kind kind;
+			kind = p->stack[p->sp].kind;
+			p->sp++;
+			if (kind == nk_right_paren)
+				proc_right_paren(p);
+			else if (kind == nk_left_paren)
+				proc_left_paren(p);
+			else if (kind == nk_left_brace)
+				proc_left_brace(p);
+			else if (kind == nk_right_brace)
+				proc_right_brace(p);
+			else if (kind == nk_semi)
+				proc_semi(p);
+		}
+	}
+}
+
+void cmain(long argc, char **argv) {
+	long fd;
+	long size;
+	long debug;
+	long r;
+	struct lexer l;
+	struct parser p;
+
+	debug = 0;
+
+	if (argc < 2) {
+		write_str(2, "Usage: famc <file>\n");
+		exit_group(-1);
+	}
+
+	if (argc > 2) {
+		cstrcmp(&r, argv[1], "--debug");
+		if (r == 0) debug = 1;
+	}
+
+	open(&fd, argv[argc - 1], 0, 0);
+	if (fd < 0) {
+		write_str(2, "Could not open file!\n");
+		exit_group(-1);
+	}
+
+	lseek(&size, fd, 0, 2);
+	if (size < 0) {
+		write_str(2, "Could not determine file size!\n");
+		exit_group(-1);
+	}
+
+	fmap_ro((void *)&(&l)->in, fd, size, 0);
+	if (l.in == 0) {
+		write_str(2, "Could not mmap file!\n");
+		exit_group(-1);
+	}
+	close(&r, fd);
+
+	arena_init(&p.a, 256 * 1024 * 1024, 8);
+	node_init(&p, &p.root, nk_program);
+	p.in = l.in;
+	p.current = p.root;
+	p.sp = 0;
+	p.capacity = 4096;
+	map((void *)&p.stack, sizeof(struct node) * 4096);
+	if (!p.stack) panic("could not allocate stack");
+
+	l.end = l.in + size;
+	l.off = 0;
+	l.col_start = 0;
+	l.line = 0;
+
+	parse(&p, &l, debug);
+	if (debug) node_print(&p, p.root);
+
+	if (debug) write_str(1, "success!\n");
 
 	exit_group(0);
-
-	(void)argv;
-	(void)argc;
 }
